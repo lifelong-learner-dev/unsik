fd83d774514aa02c1e552cd310d2f3e9c3841975
LuenArstery04 <csh98.dev@gmail.com>
2024-01-11 22:31:25 +0900

    입력창 추가/삭제 기능 우선 구현

--- static/js/meal_anal_result.js
+++ static/js/meal_anal_result.js
@@ -1,30 +1,19 @@
 $(document).ready(function(){
+    // remove_input 클래스가 부여된 버튼을 클릭했을 때
     $('#meal_result_form').on('click', '.remove_input', function(event) {
-        const inputContainer = $(event.target).parent();
-
-        const csrf_token = $('input[name="csrfmiddlewaretoken"]').val();
-
-        $.ajax({
-            url: 'http://127.0.0.1:8000/meal/meal_to_analyze',
-            type: 'POST',
-            data: {
-                csrfmiddlewaretoken: csrf_token,
-            },
-            success: function(response) {
-                inputContainer.remove();
-            },
-            error: function (error) {
-                console.error('CSRF 토큰 오류 발생', error);
-            }
-        });
+        // 해당 개체의 이전 값 input 태그를 지우고
+        // 자기 자신도 지워버린다.
+        $(this).prev('input').remove();
+        $(this).remove();
     });
 
+    // add_input_box 즉 '입력창 추가' 버튼을 클릭했을 때
     $('#add_input_box').on('click', function(event) {
         const last_input = $('#meal_result_form').find(':input:last');
 
         const add_input = $('<input>', {
             type: 'text',
-            name: 'food_info'
+            name: 'food_info[]'
         });
 
         const remove_btn = $('<button>', {
@@ -33,5 +22,25 @@ $(document).ready(function(){
         });
 
         last_input.after(add_input, remove_btn);
+    });
+
+    $('#submit_form').on('click', function() {
+        const form = $('#meal_result_form');
+        const formData = form.serialize();
+        const csrf_token = $('input[name="csrfmiddlewaretoken"]').val();
+
+        $.ajax({
+            url: 'http://127.0.0.1:8000/meal/meal_to_analyze',
+            type: 'POST',
+            data: formData + '&csrfmiddlewaretoken=' + csrf_token,
+            contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+            processData: false,
+            success: function(response) {
+                console.log('성공', response)
+            },
+            error: function (error) {
+                console.error('CSRF 토큰 오류 발생', error);
+            }
+        });
     });
 });
