4845bcb3c93ec3f683a2398734f0935d524e080d
lifelong-learner-dev <dev.lifelong.learner.dev@gmail.com>
2024-01-23 12:00:14 +0300

    마이페이지 백엔드+프론트엔드 1차 완료

--- users_app/forms.py
+++ users_app/forms.py
@@ -40,3 +40,39 @@ class UserForm(UserCreationForm):
             )
 
         return user
+    
+
+class MypageUserForm(forms.ModelForm):
+    user_activity = forms.ChoiceField(
+        label="활동수준",
+        choices=(("active", "활동적"), ("deactive", "비활동적")),
+        widget=forms.RadioSelect  # 라디오 버튼 형태로 선택하도록 설정
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            'user_height',
+            'user_weight',
+            'user_activity',
+            'user_target_weight',
+        )
+        labels = {
+            'user_height': '키',
+            'user_weight': '몸무게',
+            'user_activity': '활동수준',
+            'user_target_weight': '목표몸무게',        
+        }
+
+    def save(self, commit=True):
+        user = super(MypageUserForm, self).save(commit=commit)
+
+        # Daily 레코드 생성
+        if commit:
+            Daily.objects.create(
+                user=user,
+                current_weight=user.user_weight,
+                date=timezone.now().date()
+            )
+
+        return user
