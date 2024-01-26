57ba0b0865f615de2f94661a600a83096f88b3bf
sungjoon98 <happypigs7.dev@gmail.com>
2024-01-26 16:16:01 +0900

    01/26

--- exercise/urls.py
+++ exercise/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('record_exercise/', views.record_exercise, name='record_exercise'),
     path("", views.exercise_index, name='exercise_index'),
-    path('record/', views.record_exercise, name='record_exercise'),
 ]
